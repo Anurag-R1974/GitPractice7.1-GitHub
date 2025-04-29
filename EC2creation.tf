249421891e402cf1b4b2c3bed33e657c90c9dfe7
root <root@ip-172-31-18-61.ec2.internal>
2025-04-29 16:41:11 +0000

    micro to large 1st commit by linux

---
+++ EC2creation.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "question" {
+  ami = "ami-0e449927258d45bc4"
+
+  instance_type = "t2.large"
+
+
+  tags = {
+    Name = "GitPractice1"
+  }
+}
+
+
+# micro to nano 1st commit by windows
+
+# micro to large 1st commit by linux
+# Linux 1st commit 5e5d58d18a84661b5b2f37a0f191c872dbd4dad6
