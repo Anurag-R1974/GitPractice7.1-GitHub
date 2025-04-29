36de311d10f0022d56610a7bbdbab5cd76cf39fb
Anurag-R1974 <sanuragramagiri@gmail.com>
2025-04-29 21:40:36 +0530

    micro to nano 1st commit by windows

--- EC2creation.tf
+++ EC2creation.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "question" {
   ami = "ami-0e449927258d45bc4"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
   tags = {
     Name = "GitPractice1"
   }
 }
+
+# micro to nano 1st commit by windows
