5e5d58d18a84661b5b2f37a0f191c872dbd4dad6
root <root@ip-172-31-18-61.ec2.internal>
2025-04-29 16:08:15 +0000

    micro to large 1st commit by linux

--- EC2creation.tf
+++ EC2creation.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "question" {
   ami = "ami-0e449927258d45bc4"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   tags = {
     Name = "GitPractice1"
   }
 }
+# micro to large 1st commit by linux
